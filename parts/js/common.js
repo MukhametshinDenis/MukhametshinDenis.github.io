c62c7e8d50831272de5946e61e5b1d9302d4ed8d
MukhametshinDenis <123456789mdf@mail.ru>
2018-04-12 22:09:33 +0500

    parts

--- parts/js/common.js
+++ parts/js/common.js
@@ -1,5 +1,11 @@
 $(function() {
 
+	//прелодер
+	$(window).load(function() { 
+		$(".loaderInner").fadeOut(); 
+		$(".loader").delay(400).fadeOut("slow"); 
+	});
+
 	// Маска на input
 	jQuery(function($){
    $(".phone").mask("+7 (999) 999-99-99");
@@ -58,5 +64,43 @@ $(function() {
 		return false;
 	});
 
+	//Запуск прелодер
+	var form = $('#test_form');
+	form.preloader('start');
+	$.post('/backend/', form.serialize())
+	.done(function() { /*...*/ })
+	.fail(function() { /*...*/ })
+	.always(function() {
+			form.preloader('stop');
+	});
 
 });
+
+	//плагин прелодер
+(function($) {
+    var actions = {
+        start: function() {
+            var $preloader = $("<div id='jpreloader' class='preloader-overlay'><div class='loader' style='position:absolute;left:50%;top:50%;margin-left:-24px;margin-top:-24px;'><svg width='48px' height='48px' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100' preserveAspectRatio='xMidYMid' class='uil-default'>...</svg></div></div>");
+            $preloader.css({
+                'background-color': '#4c4c4c',
+                'width': '100%',
+                'height': '100%',
+                'left': '0',
+                'top': '0',
+                'opacity': '0.3',
+                'z-index': '100',
+                'position': 'absolute'
+            });
+            this.append($preloader);
+        },
+
+        stop: function() {
+            this.find('.preloader-overlay').remove();
+        }
+    };
+    
+    $.fn.preloader = function(action) {        
+        actions[action].apply(this);
+        return this;
+    };
+}(jQuery));
