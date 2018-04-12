4c12b1eb8ce36545c37f3f71f9aa9a5f9b82b4de
MukhametshinDenis <123456789mdf@mail.ru>
2018-04-12 22:49:01 +0500

    parts

--- parts/js/common.js
+++ parts/js/common.js
@@ -1,11 +1,5 @@
 $(function() {
 
-	//прелодер
-	$(window).load(function() { 
-		$(".loaderInner").fadeOut(); 
-		$(".loader").delay(400).fadeOut("slow"); 
-	});
-
 	// Маска на input
 	jQuery(function($){
    $(".phone").mask("+7 (999) 999-99-99");
@@ -48,7 +42,7 @@ $(function() {
 		placemark.name = "г.Астана, ул. Куйши Дина 28";
 		map.addOverlay(placemark); 
 
-	})
+	});
 
 	//Форма обатной формы
 	$("#form").submit(function() {
@@ -65,42 +59,8 @@ $(function() {
 	});
 
 	//Запуск прелодер
-	var form = $('#test_form');
-	form.preloader('start');
-	$.post('/backend/', form.serialize())
-	.done(function() { /*...*/ })
-	.fail(function() { /*...*/ })
-	.always(function() {
-			form.preloader('stop');
+	$(window).on('load', function() {
+		$('.load').delay(100).fadeOut('slow');
 	});
 
 });
-
-	//плагин прелодер
-(function($) {
-    var actions = {
-        start: function() {
-            var $preloader = $("<div id='jpreloader' class='preloader-overlay'><div class='loader' style='position:absolute;left:50%;top:50%;margin-left:-24px;margin-top:-24px;'><svg width='48px' height='48px' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100' preserveAspectRatio='xMidYMid' class='uil-default'>...</svg></div></div>");
-            $preloader.css({
-                'background-color': '#4c4c4c',
-                'width': '100%',
-                'height': '100%',
-                'left': '0',
-                'top': '0',
-                'opacity': '0.3',
-                'z-index': '100',
-                'position': 'absolute'
-            });
-            this.append($preloader);
-        },
-
-        stop: function() {
-            this.find('.preloader-overlay').remove();
-        }
-    };
-    
-    $.fn.preloader = function(action) {        
-        actions[action].apply(this);
-        return this;
-    };
-}(jQuery));
